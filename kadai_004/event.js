225f4944273ba285dc00e8ffa5e85f96f69efe1e
pikariru <167439420+pikariru@users.noreply.github.com>
2024-09-07 13:39:46 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // ページロード時の処理
+    console.log("loadイベントが発生しました");
+  
+    // スクロールイベントの処理
+    $(window).scroll(function() {
+      console.log("scrollイベントが発生しました");
+    });
+  });
